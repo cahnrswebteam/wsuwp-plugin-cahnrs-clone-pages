2d258dc399bdcdf47c8e0db6a0de531c5954521c
Efren Vasquez <efren.vasquez@wsu.edu>
2024-07-16 10:52:33 -0500

    fix: allow forms to be cloned

--- classes/class-clone-pages.php
+++ classes/class-clone-pages.php
@@ -26,64 +26,125 @@ class CAHNRS_Clone_Pages {
     private function handle_clone_pages() {
         $selected_site = isset($_POST['selected_site']) ? intval($_POST['selected_site']) : 0;
         $selected_pages = isset($_POST['pages_to_clone']) ? $_POST['pages_to_clone'] : array();
+        $selected_forms = isset($_POST['forms_to_clone']) ? $_POST['forms_to_clone'] : array();
+        $cloned_pages = array();
+        $cloned_forms = array();
 
         echo "Selected Site: $selected_site<br>";
         echo "Selected Pages: " . implode(', ', $selected_pages) . "<br>";
+        echo "Selected Forms: " . implode(', ', $selected_forms) . "<br>";
 
-        if ($selected_site && !empty($selected_pages)) {
+        if ($selected_site && (!empty($selected_pages) || !empty($selected_forms))) {
             switch_to_blog($selected_site);
             echo "Switched to site ID: $selected_site<br>";
 
-            $parent_pages_to_clone = array();
-            foreach ($selected_pages as $page_id) {
-                $this->clone_ancestor_pages($page_id, $parent_pages_to_clone);
+            if (!empty($selected_pages)) {
+                $parent_pages_to_clone = array();
+                foreach ($selected_pages as $page_id) {
+                    $page = get_post($page_id);
+                    $this->clone_ancestor_pages($page->ID, $parent_pages_to_clone);
+                }
+
+                $selected_pages = array_merge($selected_pages, $parent_pages_to_clone);
+                $selected_pages = array_unique($selected_pages);
+
+                $new_page_id_menu_items = array();
+
+                foreach ($selected_pages as $page_id) {
+                    $page = get_post($page_id);
+                    if ($page) {
+                        echo "Cloning page ID: $page_id from site ID: $selected_site<br>";
+
+                        $updated_content = wp_filter_content_tags($page->post_content);
+
+                        $updated_content = preg_replace('/\s(decoding="async"|srcset="[^"]*"|loading="[^"]*"|sizes="[^"]*"|width="[^"]*"|height="[^"]*")/', '', $updated_content);
+
+                        preg_match_all('/<img.*?src=["\'](.*?)["\'].*?>/i', $updated_content, $matches);
+
+                        $updated_content = str_replace("\\", "\\\\", $updated_content);
+                        $original_parent_title = get_post_field('post_title', $page->post_parent);
+
+                        restore_current_blog();
+
+                        $updated_content = preg_replace('/"imageId":\d+,?/', '', $updated_content);
+
+                        $new_page = array(
+                            'post_title' => $page->post_title,
+                            'post_content' => $updated_content,
+                            'post_status' => 'draft',
+                            'post_type' => 'page'
+                        );
+                        $new_page_id = wp_insert_post($new_page);
+
+                        $new_page_id_menu_items[] = $new_page_id;
+
+                        if ($new_page_id) {
+                            echo "Page cloned successfully. New page ID: $new_page_id<br>";
+
+                            if ($page->post_parent) {
+                                $new_parent = get_page_by_title($original_parent_title, OBJECT, 'page');
+                                if ($new_parent) {
+                                    wp_update_post(array(
+                                        'ID' => $new_page_id,
+                                        'post_parent' => $new_parent->ID
+                                    ));
+                                    echo "Parent page set for cloned page.<br>";
+                                } else {
+                                    echo "Parent page not found for cloned page.<br>";
+                                }
+                            }
+
+                            $cloned_pages[] = $page_id;
+                        } else {
+                            echo "Failed to clone page.<br>";
+                        }
+
+                        switch_to_blog($selected_site);
+                    }
+                }
+
+                restore_current_blog();
+
+                $this->create_menu_from_cloned_pages($new_page_id_menu_items);
             }
 
-            $selected_pages = array_unique(array_merge($selected_pages, $parent_pages_to_clone));
-
-            $new_page_id_menu_items = array();
-
-            foreach ($selected_pages as $page_id) {
-                $page = get_post($page_id);
-                if ($page) {
-                    echo "Cloning page ID: $page_id from site ID: $selected_site<br>";
-                    $updated_content = $this->clean_content($page->post_content);
-                    $original_parent_title = get_post_field('post_title', $page->post_parent);
-
+            if (!empty($selected_forms)) {
+                foreach ($selected_forms as $form_id) {
+                    switch_to_blog($selected_site);
+                    $form = GFAPI::get_form($form_id);
                     restore_current_blog();
 
-                    $new_page_id = $this->clone_page($page, $updated_content, $original_parent_title);
-                    if ($new_page_id) {
-                        $new_page_id_menu_items[] = $new_page_id;
-                        $cloned_pages[] = $page_id;
-                    }
+                    if ($form) {
+                        $existing_forms = GFAPI::get_forms();
+                        $form_exists = false;
+                        foreach ($existing_forms as $existing_form) {
+                            if ($existing_form['title'] === $form['title']) {
+                                $form_exists = true;
+                                break;
+                            }
+                        }
 
-                    switch_to_blog($selected_site);
+                        if ($form_exists) {
+                            echo 'Form "' . $form['title'] . '" already exists. Skipping cloning.<br>';
+                        } else {
+                            $form['title'] = $form['title'];
+                            $new_form_id = GFAPI::add_form($form);
+
+                            if (is_wp_error($new_form_id)) {
+                                echo 'Failed to clone form ID ' . $form_id . ': ' . $new_form_id->get_error_message() . '<br>';
+                            } else {
+                                echo 'Form cloned successfully. New form ID: ' . $new_form_id . '<br>';
+                                $cloned_forms[] = $form_id;
+                            }
+                        }
+                    } else {
+                        echo "Failed to get form ID $form_id.<br>";
+                    }
                 }
             }
-
-            restore_current_blog();
-            $this->create_menu_from_cloned_pages($new_page_id_menu_items);
         } else {
-            echo "No selected pages to clone.<br>";
+            echo "No selected pages or forms to clone.<br>";
         }
-    }
-
-    private function clean_content($content) {
-        $content = wp_filter_content_tags($content);
-        $content = preg_replace('/\s(decoding="async"|srcset="[^"]*"|loading="[^"]*"|sizes="[^"]*"|width="[^"]*"|height="[^"]*")/', '', $content);
-        $content = str_replace("\\", "\\\\", $content);
-
-        preg_match_all('/"imageId"\s*:\s*(\d+).*?"imageSrc"\s*:\s*"(.*?)"/s', $content, $block_matches, PREG_SET_ORDER);
-
-        foreach ($block_matches as $match) {
-            $image_id = intval($match[1]);
-            $image_url = $match[2];
-            $image_id = attachment_url_to_postid($image_url);
-            $content = preg_replace('/"imageId":\d+/', '"imageId":' . $image_id, $content);
-        }
-
-        return $content;
     }
 
     private function clone_page($page, $updated_content, $original_parent_title) {
@@ -171,15 +232,13 @@ class CAHNRS_Clone_Pages {
                 }
             }
     
-            echo 'Menu created successfully.<br>';
+            echo 'Menu created and pages added successfully.<br>';
         } else {
-            echo 'No pages cloned to create menu.<br>';
+            echo 'No pages to add to menu.<br>';
         }
     }
-    
-    
 
-    public function display_settings_page() {
+    private function display_settings_page() {
         ?>
         <div class="wrap">
             <h2>CAHNRS Clone Pages Settings</h2>
@@ -215,18 +274,24 @@ class CAHNRS_Clone_Pages {
             switch_to_blog($selected_site);
             $site_title = get_blog_details($selected_site)->blogname;
             $pages = get_posts(array('post_type' => 'page', 'posts_per_page' => -1, 'post_status' => 'publish'));
+            $forms = GFAPI::get_forms();
 
-            if ($pages) {
-                echo '<h3>' . esc_html($site_title) . ' Pages:</h3>';
+            if ($pages || $forms) {
+                echo '<h3>' . esc_html($site_title) . ' Pages and Forms:</h3>';
                 echo '<form method="post" action="">';
                 $selected_pages = isset($_POST['pages_to_clone']) ? $_POST['pages_to_clone'] : array();
                 $this->display_pages_with_hierarchy($pages, $selected_pages);
+                if (!empty($forms)) {
+                    echo '<h3>' . esc_html($site_title) . ' Forms:</h3>';
+                    $selected_forms = isset($_POST['forms_to_clone']) ? $_POST['forms_to_clone'] : array();
+                    $this->display_forms($forms, $selected_forms);
+                }
                 echo '<input type="submit" name="clone_pages" class="button-primary" value="Clone Pages">';
                 echo '<input type="hidden" name="selected_site" value="' . esc_attr($selected_site) . '">';
                 echo '<input type="hidden" name="action" value="clone_pages">';
                 echo '</form>';
             } else {
-                echo 'No pages found on this site.<br>';
+                echo 'No pages or forms found on this site.<br>';
             }
             restore_current_blog();
         }
@@ -247,6 +312,13 @@ class CAHNRS_Clone_Pages {
             echo '<label><input type="checkbox" name="pages_to_clone[]" value="' . esc_attr($page->ID) . '" ' . $selected . '>' . esc_html($page->post_title) . '</label><br>';
             $this->display_pages_with_hierarchy($pages, $selected_pages, $indent + 1, $page->ID, $displayed_pages);
             $displayed_pages[] = $page->ID;
+        }
+    }
+
+    private function display_forms($forms, $selected_forms = array()) {
+        foreach ($forms as $form) {
+            $selected = is_array($selected_forms) && in_array($form['id'], $selected_forms) ? 'checked' : '';
+            echo '<label><input type="checkbox" class="form-checkbox" name="forms_to_clone[]" value="' . esc_attr($form['id']) . '" ' . $selected . '>' . esc_html($form['title']) . '</label><br>';
         }
     }
 }
